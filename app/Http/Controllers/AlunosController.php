ad3b32474fdb8e60d9882fcd10ed10cf46e27062
bmarchete <bmarchete@gmail.com>
2018-03-02 14:17:22 -0300

    first commit

---
+++ app/Http/Controllers/AlunosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Aluno;
+
+class AlunosController extends Controller
+{
+    public function index()
+    {
+        $dados = Aluno::all();
+        return view('alunos', compact('dados'));
+    }
+}
